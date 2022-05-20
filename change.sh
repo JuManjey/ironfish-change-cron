326c9c3f86f9b61928aaa7fe5e74c2ba201584ce
JuManji <opghld@gmail.com>
2022-05-20 18:37:50 +0300

    Update change.sh

--- change.sh
+++ change.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sed -i '$d' /etc/crontab
-*/1 * * * * root bash /root/ironfish-scripts/deposit.sh | tee -a /root/logfile.log >> /etc/crontab
+echo "*/1 * * * * root bash /root/ironfish-scripts/deposit.sh | tee -a /root/logfile.log" >> /etc/crontab
