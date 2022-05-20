0eec1f278d79811113959de18011bef54a29d963
JuManji <opghld@gmail.com>
2022-05-20 17:57:21 +0300

    Update change.sh

--- change.sh
+++ change.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sed -i '$d' /etc/crontab
-echo "*/1 * * * * root python3 /root/ironfish-scripts/deposit.sh | tee -a /root/logfile.log" >> /etc/crontab
+echo "*/1 * * * * root bash /root/ironfish-scripts/deposit.sh | tee -a /root/logfile.log" >> /etc/crontab
